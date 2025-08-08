78bf622fc3a6844efcbf9b373c9f268fe46af9ae
eebbaaf <eebbaa84@gmail.com>
2025-08-08 00:23:53 -0700

    Add basic environment structure

---
+++ environments/prisoners_dilemma.py
@@ -0,0 +1,15 @@
+import numpy as np
+from pettingzoo import ParallelEnv
+
+class PrisonersDilemmaEnv(ParallelEnv):
+    def __init__(self, max_rounds=100, observation_window=5):
+        # TODO: implement game logic
+        pass
+    
+    def reset(self, seed=None, options=None):
+        # TODO: implement reset
+        pass
+    
+    def step(self, actions):
+        # TODO: implement step
+        pass
